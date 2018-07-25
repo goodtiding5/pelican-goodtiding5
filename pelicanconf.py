62aac0131c695b43dd2fa970e0bf374dee33521c
Kenneth Zhao <kenneth.zhao@gmail.com>
2018-07-24 23:52:31 -0700

    Added and activated pelican_bootstrap3 theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,15 @@ DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Plugins and Theme
+
+PLUGIN_PATHS = ['pelican-plugins']
+
+THEME = 'pelican-themes/pelican-bootstrap3'
+BOOTSTRAP_THEME = 'flatly'
+
+JINJA_ENVIRONMENT = {'extensions' : ['jinja2.ext.i18n']}
+PLUGINS = ['i18n_subsites']
+
+I18N_TEMPLATES_LANG = 'en'
